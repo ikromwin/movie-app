f19a84bd4214badace2b6900b022d9cfe5d64278
uikromm <u.ikromm@gmail.com>
2025-03-14 16:23:24 +0500

    dotenv

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-const API_KEY = "3e34fca40f9822d5ea4ded0406757754";
-const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
